fd59fda1783488b0051d3f79d07c157a9d8d05fc
DevM7mdAli <74423891+DevM7mdAli@users.noreply.github.com>
2026-02-13 10:00:39 +0300

    fix: missing inisilize into the prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit(): Promise<void> {
+  constructor() {
+    // 1. Create a standard PostgreSQL connection pool
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+    // 2. Create the Prisma Adapter using that pool
+    const adapter = new PrismaPg(pool);
+
+    // 3. Pass the adapter to the parent constructor
+    super({ adapter });
+  }
+
+  async onModuleInit() {
     await this.$connect();
   }
 }
